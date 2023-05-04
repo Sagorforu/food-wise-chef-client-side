4c8275261d3d3516be3a3130f49a2ec431c74b8c
sagorforu <freelancersagor18@gmail.com>
2023-05-04 23:21:42 +0600

    user info page loading error fixed

--- src/components/Register/UserDetails.jsx
+++ src/components/Register/UserDetails.jsx
@@ -1,9 +1,33 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../Providers/AuthProvider";
+import { ColorRing } from "react-loader-spinner";
 
 const UserDetails = () => {
-  const { user } = useContext(AuthContext);
+  const { user, loading } = useContext(AuthContext);
+  console.log(user, loading);
 
+  if (loading) {
+    return (
+      <div className="flex justify-center">
+        <ColorRing
+          visible={true}
+          height="80"
+          width="80"
+          ariaLabel="blocks-loading"
+          wrapperStyle={{}}
+          wrapperClass="blocks-wrapper"
+          colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]}
+        />
+      </div>
+    );
+  }
+  if (!user) {
+    return (
+      <h1 className="text-[#383838] text-center font-bold text-3xl lg:text-4xl">
+        No user found
+      </h1>
+    );
+  }
 
   return (
     <div className="lg:px-36 px-2 ">
@@ -13,9 +37,13 @@ const UserDetails = () => {
         </h2>
       </div>
       <div className="mt-8">
-      <img className="mx-auto w-4/12 rounded-xl" src={user.photoURL} alt="" />
-      <h2 className="text-center text-[#383838] mt-6 mb-2 font-bold text-2xl">{user.displayName}</h2>
-      <h2 className="text-center text-[#383838] mb-6 font-semibold text-xl">{user.email}</h2>
+        <img className="mx-auto w-4/12 rounded-xl" src={user.photoURL} alt="" />
+        <h2 className="text-center text-[#383838] mt-6 mb-2 font-bold text-2xl">
+          {user.displayName}
+        </h2>
+        <h2 className="text-center text-[#383838] mb-6 font-semibold text-xl">
+          {user.email}
+        </h2>
       </div>
     </div>
   );
