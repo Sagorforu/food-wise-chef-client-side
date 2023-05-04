90771c10deda1dc3079d593c32977eb01f3f5738
sagorforu <freelancersagor18@gmail.com>
2023-05-04 22:34:29 +0600

    user info added

--- src/components/Home/BookNow.jsx
+++ src/components/Home/BookNow.jsx
@@ -12,7 +12,11 @@ const BookNow = () => {
       <div className="mt-6 grid lg:grid-cols-2 gap-4 items-center justify-between lg:px-36 px-2">
         <div>
           <div className="flex justify-center">
-            <img className="rounded-2xl" src="https://i.ibb.co/xJcFScw/picture3.jpg" alt="" />
+            <img
+              className="rounded-2xl"
+              src="https://i.ibb.co/xJcFScw/picture3.jpg"
+              alt=""
+            />
           </div>
         </div>
         <div>
@@ -20,16 +24,18 @@ const BookNow = () => {
             Food Wise Restaurant
           </h3>
           <p className="text-[#383838] text-base font-medium mt-2">
-            The quote "First, we eat. Then, we do everything else" by M.F.K.
-            Fisher encapsulates the idea that food is not just a means of
-            sustenance but a fundamental part of our daily lives. It highlights
-            the importance of nourishing ourselves before we can tackle the
-            demands of the day ahead.
+            "Feast for the Senses" - This title promises an immersive culinary
+            experience that tantalizes all five senses. From the aroma of
+            freshly baked bread to the colorful presentation of each dish, every
+            element is carefully crafted to delight the eyes, nose, mouth, ears,
+            and even the touch of diners.
             <br />
-            This quote reminds us to prioritize our health and well-being, to
-            savor our meals, and to take pleasure in the simple act of eating.
-            It is a powerful reminder that food is more than just nourishment,
-            but a gateway to a fulfilling life.
+            From the aroma of freshly baked bread to the colorful presentation
+            of each dish, every element is carefully crafted to delight the
+            eyes, nose, mouth, ears, and even the touch of diners, and to take
+            pleasure in the simple act of eating. It is a powerful reminder that
+            food is more than just nourishment, but a gateway to a fulfilling
+            life.
           </p>
           <Link to="/booknow">
             <button className="btn btn-outline mt-6 text-[#383838]">
--- src/components/Register/UserDetails.jsx
+++ src/components/Register/UserDetails.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AuthContext } from "../Providers/AuthProvider";
 
 const UserDetails = () => {
+  const { user } = useContext(AuthContext);
+
+
   return (
     <div className="lg:px-36 px-2 ">
       <div className="background-color text-center pb-8 lg:pt-12 pt-4">
-        <h2 className="text-[#1A1919] font-bold text-3xl lg:text-4xl">
+        <h2 className="text-[#383838] font-bold text-3xl lg:text-4xl">
           User Information
         </h2>
+      </div>
+      <div className="mt-8">
+      <img className="mx-auto w-4/12 rounded-xl" src={user.photoURL} alt="" />
+      <h2 className="text-center text-[#383838] mt-6 mb-2 font-bold text-2xl">{user.displayName}</h2>
+      <h2 className="text-center text-[#383838] mb-6 font-semibold text-xl">{user.email}</h2>
       </div>
     </div>
   );
